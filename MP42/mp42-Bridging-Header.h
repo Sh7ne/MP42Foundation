1c78316d92aead94126175ea2556d8317220ca2d
Yao Zhao <shinenirvana@gmail.com>
2026-06-06 07:12:02 +0800

    Support macOS 26 bridging header imports and update deployment target

--- MP42/mp42-Bridging-Header.h
+++ MP42/mp42-Bridging-Header.h
@@ -6,5 +6,24 @@
 #ifndef mp42_Bridging_Header_h
 #define mp42_Bridging_Header_h
 
+#import "MP42File.h"
+#import "MP42FileImporter.h"
+#import "MP42Track.h"
+#import "MP42VideoTrack.h"
+#import "MP42AudioTrack.h"
+#import "MP42ChapterTrack.h"
+#import "MP42SubtitleTrack.h"
+#import "MP42ClosedCaptionTrack.h"
+#import "MP42Metadata.h"
+#import "MP42MetadataItem.h"
+#import "MP42Logging.h"
+#import "MP42Utilities.h"
+#import "NSString+MP42Additions.h"
+#import "MP42MediaFormat.h"
+#import "MP42Languages.h"
+#import "MP42RelatedItem.h"
+#import "MP42SecurityAccessToken.h"
+#import "MP42MasteringDisplayMetadata.h"
+#import "MP42Rational.h"
 
 #endif /* mp42_Bridging_Header_h */
